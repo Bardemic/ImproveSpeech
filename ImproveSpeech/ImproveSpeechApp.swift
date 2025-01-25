788f2f2ce0fcc2e6ecf3b0d5b75c59f579b5a846
Bardemic <134565367+Bardemic@users.noreply.github.com>
2025-01-25 01:36:20 -0600

    initial code

---
+++ ImproveSpeech/ImproveSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImproveSpeechApp.swift
+//  ImproveSpeech
+//
+//  Created by Brandon Pieczka on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ImproveSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
